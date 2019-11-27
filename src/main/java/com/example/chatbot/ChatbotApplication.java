c8bd9c07b347f1bd83b05626db3f42b3bcba4052
fxlmo <josh.felmeden@outlook.com>
2019-11-27 15:37:21 +0000

    Fully functioning tf-idf algorithm

--- src/main/java/com/example/chatbot/ChatbotApplication.java
+++ src/main/java/com/example/chatbot/ChatbotApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ChatbotApplication implements CommandLineRunner{
@@ -20,29 +26,97 @@ public class ChatbotApplication implements CommandLineRunner{
 
 	public void run(String... args) throws Exception {
 
-		repository.deleteAll();
+		//repository.deleteAll();
 
 		// save a couple of customers
-		repository.save(new Customer("Alice", "Smith"));
-		repository.save(new Customer("Bob", "Smith"));
+		//repository.save(new Customer("Alice", "Smith"));
+		//repository.save(new Customer("Bob", "Smith"));
 
 		// fetch all customers
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (Customer customer : repository.findAll()) {
-			System.out.println(customer);
-		}
-		System.out.println();
+		//System.out.println("Customers found with findAll():");
+		//System.out.println("-------------------------------");
+		//for (Customer customer : repository.findAll()) {
+		//	System.out.println(customer);
+		//}
+		//System.out.println();
 
 		// fetch an individual customer
-		System.out.println("Customer found with findByFirstName('Alice'):");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByFirstName("Alice"));
+		//System.out.println("Customer found with findByFirstName('Alice'):");
+		//System.out.println("--------------------------------");
+		//System.out.println(repository.findByFirstName("Alice"));
 
-		System.out.println("Customers found with findByLastName('Smith'):");
-		System.out.println("--------------------------------");
-		for (Customer customer : repository.findByLastName("Smith")) {
-			System.out.println(customer);
+		//System.out.println("Customers found with findByLastName('Smith'):");
+		//System.out.println("--------------------------------");
+		//for (Customer customer : repository.findByLastName("Smith")) {
+		//	System.out.println(customer);
+		//}
+
+		// Keyword stuff
+		// TODO: parse into thread id, date, etc. This part of the algorithm should only receive
+		// TODO: the body of each thread to get keywords out
+		ArrayList<ArrayList<String>> documents = new ArrayList<ArrayList<String>>();
+		final File folder = new File("./threads/");
+		documents = listFilesForFolder(folder);
+
+		//loop through each document and calculate td-idf of each keyword (in theory)
+		// for now I'm just going to look at one document and see what happens
+
+		//remove punctuation from each word in the document (makes things easier to process)
+		int i = 0;
+		ArrayList<String> newDocument = new ArrayList<String>();
+		for (ArrayList<String> document : documents) {
+			for (String paras : document) {
+				String[] wordList = paras.split(" ");
+				for (String word : wordList) {
+					newDocument.add(word.replaceAll("[^a-zA-Z0-9]", ""));
+				}
+			}
+			documents.set(i, new ArrayList<>(newDocument));
+			newDocument.clear();
+			i++;
 		}
+
+		TFIDFCalc keywordCalc = new TFIDFCalc();
+		final int testIndex = 4;
+		System.out.println(documents.get(testIndex));
+		ArrayList<String> keyWords = new ArrayList<>();
+		for (String word : documents.get(testIndex)) {
+			double freq = keywordCalc.tfidf(documents, documents.get(testIndex), word);
+			System.out.println("TF-IDF of " + word + " = " + freq);
+			if (freq > 1.5) {
+				keyWords.add(word);
+			}
+		}
+
+		System.out.println("keywords are " + keyWords);
 	}
+
+	public ArrayList<ArrayList<String>> listFilesForFolder(final File folder) {
+		ArrayList<ArrayList<String>> documents = new ArrayList<ArrayList<String>>();
+		for (final File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				listFilesForFolder(fileEntry);
+			} else {
+			    //read each file read into document structure
+				ArrayList<String> document = new ArrayList<String>();
+
+				try {
+					BufferedReader br = new BufferedReader(new FileReader(fileEntry));
+
+					String st;
+					while ((st = br.readLine()) != null) {
+						document.add(st);
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				documents.add(document);
+				//System.out.println(fileEntry.getName());
+			}
+		}
+		return documents;
+	}
+
 }
--- src/main/java/com/example/chatbot/TFIDFCalc.java
+++ src/main/java/com/example/chatbot/TFIDFCalc.java
@@ -1,8 +1,10 @@
 package com.example.chatbot;
 
-import java.util.List;
+import java.util.ArrayList;
 
 //Method of analysing how frequently each word occurs and in what 'document' (thread)
+//Returned value will be higher if it appears often in the current document, and not often in others
+//Returned value will be lower if it appears in a lot of documents (ie is not unique)
 public class TFIDFCalc {
 
     // TERM FREQUENCY
@@ -11,7 +13,7 @@ public class TFIDFCalc {
     // - word: Word of which we want the tf (term frequency of
     // Returns:
     // - The frequency the word appears in the document (ignoring the case)
-    public int tf(List<String> document, String word) {
+    public int tf(ArrayList<String> document, String word) {
         int result = 0;
         for (String w : document)  {
             if (word.equalsIgnoreCase(w)) {
@@ -28,16 +30,16 @@ public class TFIDFCalc {
     // - word: Word of which we want to test the idf of
     // Returns:
     // - The relative frequency of a word in other documents (kind of like how often it appears in other documents)
-    public int idf(List<List<String>> documents, String word) {
-        int result = 0;
+    public double idf(ArrayList<ArrayList<String>> documents, String word) {
+        double result = 0;
         int n = 0; //number of documents with word in
 
-        for (List<String> d : documents) {
+        for (ArrayList<String> d : documents) {
             if (tf(d, word) > 0) {
                 n++;
             }
         }
-        result = (int)Math.log(documents.size()/n);
+        result = Math.log(documents.size()/n);
 
         return result;
     }
@@ -49,7 +51,7 @@ public class TFIDFCalc {
     // - word: word that we want to check
     // Returns:
     // - The full tf-ifd of a word in a document with respect to some documents (threads)
-    public int tfidf(List<List<String>> documents, List<String> document, String word) {
-        return tf(document, word) * idf(documents, word)
+    public double tfidf(ArrayList<ArrayList<String>> documents, ArrayList<String> document, String word) {
+        return tf(document, word) * idf(documents, word);
     }
 }
