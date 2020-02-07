61ba8c72f449e25bae37b1d6468a9bc65525bd60
Daanial <Daanial74@gmail.com>
2020-02-07 02:56:02 +0000

    Made a basic main page using html and css

--- src/main/java/com/ChatbotController/ChatbotController.java
+++ src/main/java/com/ChatbotController/ChatbotController.java
@@ -1,14 +1,30 @@
 package com.ChatbotController;
 
+import com.example.chatbot.ChatbotApplication;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class ChatbotController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "<a href='https://www.google.com'>help</a>";
+    @RequestMapping(value ="/index", method = RequestMethod.GET)
+    public String questionForm(){
+        return "questionform";
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.POST)
+    public String index(HttpServletRequest request, Model model) {
+        String questionAsked = request.getParameter("question");
+        model.addAttribute("Question", "fill in later");
+
+
+        return "hello.html";
     }
 
 }
