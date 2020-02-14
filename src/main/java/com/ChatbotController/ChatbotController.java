1bfc33655db5d56a989cd941ef097260f1c49514
Daanial <Daanial74@gmail.com>
2020-02-14 13:13:39 +0000

    java start

--- src/main/java/com/ChatbotController/ChatbotController.java
+++ src/main/java/com/ChatbotController/ChatbotController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -26,41 +24,14 @@ public class ChatbotController {
         return "index";
     }
 
+
+
     @RequestMapping(value = "/index", method = RequestMethod.POST)
-    public String index(HttpServletRequest request, Model model) throws JSONException {
-        String questionAsked = request.getParameter("question");
+    public String getSearchResultViaAjax(@RequestBody String msg1) {
 
 
-        JSONObject newOB = new JSONObject();
-        ArrayList<ArrayList<String>> documents = new ArrayList<>();
-        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://admin:chatbot123@ec2-54-198-1-3.compute-1.amazonaws.com:27017/?authSource=admin&authMechanism=SCRAM-SHA-1"));
-        ;
-        DB database = database = mongoClient.getDB("chatbot");
-        ;
-        DBCollection coll = database.getCollection("threads");
-        //This will flag as deprecated but it's fine I promise
-
-
-        //GET ALL FILES FROM MONGO
-        DBCursor cursor = coll.find(new BasicDBObject());
-        //Iterate through documents
-        for (int i = 0; i < cursor.size(); i++) {
-            DBObject currentDoc = cursor.next();
-            String body = (String) currentDoc.get("body");
-            if (body != null) {
-                ArrayList<String> newDoc = new ArrayList<>();
-                for (String word : body.split(" ")) {
-                    newDoc.add(word);
-                }
-                documents.add(newDoc);
-            }
-        }
-
-        newOB = ca.normalIO(documents, coll, questionAsked);
-        String content = newOB.get("content").toString();
-        System.out.println(content);
-        model.addAttribute("Question",content);
-
+        System.out.println("got here");
         return "index";
+
     }
 }
--- src/main/resources/static/js/chatbot.js
+++ src/main/resources/static/js/chatbot.js
@@ -13,7 +13,7 @@ function sendMessage() {
     
     printMessage(query, "user");
     clearInputField();
-    chatBotAnswer(query);
+    searchAjax(query);
 }
 
 //shows message and updates html
@@ -48,9 +48,48 @@ function gotoBottom(){
 //generate answer here using ajax request to controller
 function chatBotAnswer(msg) {
     
-    var chatBotAnswer = "test";
+    var chatBotAnswer = msg;
     printMessage(chatBotAnswer);
     gotoBottom();
+    return msg
+}
+
+
+
+
+jQuery(document).ready(function($) {
+    $("#search-form").submit(function(event) {
+        $.get("test", chatBotAnswer(data))
+        // Prevent the form from submitting via the browser.
+        //event.preventDefault();
+        //searchViaAjax(chatBotAnswer("hello"));
+
+    });
+});
+
+function searchViaAjax(msg) {
+    console.log(msg)
+    data["query"] = $("#query").val();
+
+    $.ajax({
+        type : "POST",
+        contentType : "application/json",
+        url : "/index",
+        data : JSON.stringify(data),
+        timeout : 100000,
+        success : function(data, msg) {
+
+            console.log("SUCCESS: ", data);
+            console.log("res", );
+        },
+        error : function(e) {
+            console.log("ERROR: ", e);
+            
+        },
+        done : function(e) {
+            console.log("DONE");
+        }
+    });
 }
 
 
