61ab2b78f3417a4aeee77231bb7b8518785b1f13
Daanial <daanial74@gmail.com>
2020-04-06 14:24:21 +0100

    Controller was missing

--- src/main/java/com/ChatbotController/ChatbotController.java
+++ src/main/java/com/ChatbotController/ChatbotController.java
@@ -1,30 +1,96 @@
 package com.ChatbotController;
 
+import java.util.ArrayList;
+
 import com.example.chatbot.ChatbotApplication;
-import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
+import com.mongodb.DBCollection;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ChatbotController {
 
-    @RequestMapping(value ="/index", method = RequestMethod.GET)
-    public String questionForm(){
-        return "questionform";
+    @Autowired
+    ChatbotApplication ca;
+
+    ChatbotApplication app = new ChatbotApplication();
+    DBCollection collection = app.getCollection();
+    ArrayList<ArrayList<String>> documents = app.getDocs(collection);
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String questionForm() {
+        return "index";
     }
+
+
 
     @RequestMapping(value = "/index", method = RequestMethod.POST)
-    public String index(HttpServletRequest request, Model model) {
-        String questionAsked = request.getParameter("question");
-        model.addAttribute("Question", "fill in later");
+    public ResponseEntity<JSONresponse> getSearchResultViaAjax(@RequestBody String msg1) throws JSONException {
 
 
-        return "hello.html";
+        /*
+        TODO:  - send data to chatbot application
+               - Make sure message is processed correctly
+               - send back json
+               - Process and format string in js
+         */
+
+        System.out.println("message received: " + msg1);
+
+        JSONObject response = app.normalIO(documents, collection, msg1);
+        return ResponseEntity.ok(new JSONresponse(response));
+
+
     }
 
+    private class JSONresponse {
+        public String type;
+        public ArrayList<String> content;
+
+        public JSONresponse(JSONObject s) throws JSONException {
+            this.type = (String) s.get("type");
+            this.content = new ArrayList<>();
+            if (type.equals("answer")) {
+                int i = 0;
+                JSONObject JSONcontent = (JSONObject) s.get("content");
+                while(i < JSONcontent.length()) {
+                    this.content.add((String) JSONcontent.get(String.valueOf(i)));
+                    i++;
+                }
+            } else {
+                this.content.add((String) s.get("content"));
+            }
+        }
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage() {
+        return "admin";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.POST)
+    public ResponseEntity<JSONresponse> addThreadViaAjax(@RequestBody String thread) throws JSONException {
+
+        JSONObject jsonThread = new JSONObject(thread);
+        System.out.println(jsonThread);
+
+        //app.adminAdd(collection, documents, jsonThread.getString("ID"), jsonThread.getString("SubID"), jsonThread.getString("body"), jsonThread.getString("date"), jsonThread.getString("qa") .toLowerCase());
+
+        JSONObject response = new JSONObject();
+        response.put("type","success");
+        response.put("content","null");
+        return ResponseEntity.ok(new JSONresponse(response));
+
+
+    }
+
+
 }
+
