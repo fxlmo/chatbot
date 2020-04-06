21d39c43affb36ff82cd218d45f4a23d57e86dca
Daanial <daanial74@gmail.com>
2020-04-06 14:39:01 +0100

    Fixed missing git stuff

--- src/main/resources/static/js/chatbot.js
+++ src/main/resources/static/js/chatbot.js
@@ -106,7 +106,7 @@ function searchViaAjax(msg) {
     });
 }
 
-//when add button is pressed this function in invoked
+/*//when add button is pressed this function in invoked
     $("#addThreadSubmit").click(function(event) {
         console.log("got here")
         //thread ID, subthread ID, body with radio buttons for if an entry is either a question or answer
@@ -129,14 +129,70 @@ function searchViaAjax(msg) {
         console.log(threadDetails);
         //combine all variables into Json
 
-        addThreadViaAjax(threadDetails)    
-        
+        addThreadViaAjax(threadDetails)
+
         $('#addThreadModal').modal('hide');
 
 
 
     });
 
+*/
+
+ // Example starter JavaScript for disabling form submissions if there are invalid fields
+ (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+            form.classList.add('was-validated');
+          if (form.checkValidity() === true) {
+            form.classList.remove('was-validated')
+            console.log("got here")
+            //thread ID, subthread ID, body with radio buttons for if an entry is either a question or answer
+            var ID = document.getElementById("ID-input").value;
+            var subID = document.getElementById("subID-input").value;
+            var date = document.getElementById("Date-input").value;
+            var body = document.getElementById("Body-input").value;
+            console.log("here")
+            var qa;
+            var q = $('#q input:radio:checked').val();
+            console.log("got")
+            if (q){
+                qa = "q";
+            } else { qa ="a";}
+
+            event.preventDefault();
+
+            var threadDetails = {"ID":ID, "SubID":subID, "date":date, "body":body, "qa":qa}
+
+            console.log(threadDetails);
+            //combine all variables into Json
+
+            addThreadViaAjax(threadDetails)
+
+            console.log("got here");
+            $('#addThreadModal').modal('hide');
+
+            $( "div.success" ).fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
+          }
+
+        }, false);
+      });
+    }, false);
+  })();
+
+$('#addThreadModal').on('hidden.bs.modal', function () {
+    console.log("got to reset")
+    $(this).find('form').trigger('reset');
+})
 
 
 //Post request to add a new thread
